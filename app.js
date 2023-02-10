bf286aa568c94b3b7eb4d238cc93020c085cddb2
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-10 23:28:27 +0100

    le js compte a rebour

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ function setClock(){
     const secondRatio = currentDate.getSeconds()/60;
     const minuteRatio = (secondRatio + currentDate.getMinutes())/60
     const hoursRatio = (minuteRatio + currentDate.getHours())/12
-
     
     setRotation(secondHand, secondRatio);
     setRotation(minuteHand, minuteRatio);
@@ -24,3 +23,82 @@ function setRotation(element, rotationRatio){
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
 setClock();
+
+// ********* Compte à Rebour  ***************
+const time = document.querySelector('#time'); //  affichage de l'heure 
+const content = document.querySelector(".content");
+const selectMenu = document.querySelectorAll("select");
+const setAlarmBtn = document.querySelector("#btnAlarm");
+
+let alarmTime;
+
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? '0' + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for (let i = 60; i >= 0; i--) {
+    i = i < 10 ? '0' + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for (let i = 2; i > 0; i--) {
+    let ampm = i == 1 ? 'AM' : 'PM';
+    let option = `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+setInterval(() => {
+    
+    // getting hour, minute, second
+    let currentDate = new Date();
+    let h = currentDate.getHours();
+    let m = currentDate.getMinutes();
+    let s = currentDate.getSeconds();
+    let ampm = 'AM';
+    
+    // géré changement "am / pm"
+    if(h >= 12){
+        h = h-12;
+        ampm = 'PM';
+    }
+    
+    // géré quand h === 0
+    h = h == 0 ? h = 12 : h;
+    
+    // géré quand h, m, s < 10, ajout de "0".
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    time.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    // 
+    if(alarmTime == `${h} ${m}:${ampm}`){
+        console.log('Alarme');
+    }
+
+    console.log('alarme: ' +alarmTime);
+    console.log('   :    ');
+    console.log(`${h}:${m} ${ampm}`);
+
+}, 1000);
+
+//
+function setAlarm(){
+    
+    //  get alarme
+    let alarme = `${selectMenu[0].value} ${selectMenu[1].value}:${selectMenu[2].value}`;
+    
+    if(alarme.includes("Hour") || alarme.includes("Minute") || alarme.includes("AM/PM")){
+
+        return alert("SVP, entré une heure valide !");
+    }
+
+    alarmTime = alarme;
+    content.classList.add("disabel");
+    setAlarmBtn.innerText = "Clear Alarm";
+}
+// Le button set alarm
+setAlarmBtn.addEventListener("click", setAlarm)
