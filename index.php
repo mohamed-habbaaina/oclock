0f613abb7fe7c8a3ca5118131e15a8d5661e78df
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-11 11:17:18 +0100

    html de l'alarme

--- index.php
+++ index.php
@@ -9,24 +9,50 @@
     <title>O'clock</title>
 </head>
 <body>
-    <div class="clock">
-        <div class="hand hour" data-hour-hand></div>
-        <div class="hand minute" data-minute-hand></div>
-        <div class="hand second" data-second-hand></div>
-        <div class="number number1">1</div>
-        <div class="number number2">2</div>
-        <div class="number number3">3</div>
-        <div class="number number4">4</div>
-        <div class="number number5">5</div>
-        <div class="number number6">6</div>
-        <div class="number number7">7</div>
-        <div class="number number8">8</div>
-        <div class="number number9">9</div>
-        <div class="number number10">10</div>
-        <div class="number number11">11</div>
-        <div class="number number12">12</div>
-    </div>
+    <main>
+        <!-- Horloge -->
+        <div class="clock">
+            <div class="hand hour" data-hour-hand></div>
+            <div class="hand minute" data-minute-hand></div>
+            <div class="hand second" data-second-hand></div>
+            <div class="number number1">1</div>
+            <div class="number number2">2</div>
+            <div class="number number3">3</div>
+            <div class="number number4">4</div>
+            <div class="number number5">5</div>
+            <div class="number number6">6</div>
+            <div class="number number7">7</div>
+            <div class="number number8">8</div>
+            <div class="number number9">9</div>
+            <div class="number number10">10</div>
+            <div class="number number11">11</div>
+            <div class="number number12">12</div>
+        </div>
 
+        <!-- compte a rebours -->
+        <div class="compteRebour">
+            <h1 id="time">00:00:00 PM</h1>
+            <div class="content">
+                <div class="colRebour">
+                    <select name="" id="">
+                        <option value="Hour" selected hidden>Hour</option>
+                    </select>
+                </div>
+                <div class="colRebour">
+                    <select name="" id="">
+                        <option value="Minute" selected hidden>Minute</option>
+                    </select>
+                </div>
+                <div class="colRebour">
+                    <select name="" id="">
+                        <option value="AM/PM" selected hidden>AM/PM</option>
+                    </select>
+                </div>
+
+            </div>
+            <button id="btnAlarm">Set Alarme</button>
+        </div>
+    </main>
 
 <script src="./app.js"></script>
 </body>
